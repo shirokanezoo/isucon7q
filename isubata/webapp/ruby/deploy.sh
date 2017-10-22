ce58eb37c3640db0c405d4543227d337baaae7c0
Issei Naruta <naruta@cookpad.com>
2017-10-22 16:38:50 +0900

    Truncating logs after deploy

--- isubata/webapp/ruby/deploy.sh
+++ isubata/webapp/ruby/deploy.sh
@@ -10,3 +10,5 @@ bundle check || bundle install --path ~/vendor/bundle --jobs 300
 sudo cp isubata.ruby.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl restart isubata.ruby
+
+sudo bash -c 'echo > /var/log/nginx/access.log; echo > /tmp/isu-query.log; echo > /tmp/isu-rack.log'

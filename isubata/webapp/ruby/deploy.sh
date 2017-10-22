d6f81d24632b1fdc13e6e9efd874df70a6cc409b
Sorah Fukumori <sorah@cookpad.com>
2017-10-22 15:16:34 +0900

    puma.conf.rb

--- isubata/webapp/ruby/deploy.sh
+++ isubata/webapp/ruby/deploy.sh
@@ -1,7 +1,12 @@
-#!/bin/bash -e
+#!/bin/bash -ex
+
+export PATH=/home/isucon/local/ruby-trunk/bin:$PATH
 
 cd ~/isubata/webapp/ruby/
 git pull
 mkdir ~/vendor || :
 bundle check || bundle install --path ~/vendor/bundle --jobs 300
+
+sudo cp isubata.ruby.service /etc/systemd/system/
+sudo systemctl daemon-reload
 sudo systemctl restart isubata.ruby
--- isubata/webapp/ruby/puma.conf.rb
+++ isubata/webapp/ruby/puma.conf.rb
@@ -2,6 +2,8 @@ workers 2
 threads 32,32
 
 bind 'tcp://0.0.0.0:5000'
-bind 'unix:///tmp/puma.sock'
+bind 'unix:///run/isubata/puma.sock'
+
+stdout_redirect '/home/isucon/puma.out', '/home/isucon/puma.err', true
 
 preload_app!
